a93b484a7ca4fc7f93475cf746792b1aac1ce046
QuentinIcky <quentin.ruy@gmail.com>
2025-03-03 15:50:12 +0100

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CollectionComponent } from './pages/collection/collection.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'collection', component: CollectionComponent},
     {path: '**', component: HomeComponent},
 ];
